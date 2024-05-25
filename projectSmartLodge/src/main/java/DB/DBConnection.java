a0995f492d62f19df5c0be0e2500891044513786
Yin <khangnqce180499@fpt.edu.vn>
2024-05-25 11:31:19 +0700

    update folder

---
+++ projectSmartLodge/src/main/java/DB/DBConnection.java
@@ -0,0 +1,25 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Yin
+ */
+public class DBConnection {
+
+    public static Connection getConnection() {
+        Connection conn;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://Yin:1433;databaseName=testData;user=sa;password=123;encrypt=true;trustServerCertificate=true;";
+            conn = DriverManager.getConnection(url);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            conn = null;
+        }
+        return conn;
+    }
+}
